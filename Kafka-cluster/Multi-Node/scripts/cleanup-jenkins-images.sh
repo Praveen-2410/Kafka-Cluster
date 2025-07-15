47e8072bec4ab32261bf16d16cbb9ffc3e3d5b3f
Praveen-2410 <chintalavenkatapraveenkumar@gmail.com>
2025-07-15 11:53:29 +0530

    Update cleanup-jenkins-images.sh

--- Kafka-cluster/Multi-Node/scripts/cleanup-jenkins-images.sh
+++ Kafka-cluster/Multi-Node/scripts/cleanup-jenkins-images.sh
@@ -16,7 +16,7 @@ echo "Removing ALL Jenkins images with prefix: $IMAGE_PREFIX"
 
 docker images --format '{{.Repository}}:{{.Tag}}' | grep "$IMAGE_PREFIX" | while read img; do
   echo "Removing: $img"
-  sudo docker rmi -f "$img" || echo "Failed: $img"
+  docker rmi -f "$img" || echo "Failed: $img"
 done
 
 echo "✅ Jenkins image cleanup done."
