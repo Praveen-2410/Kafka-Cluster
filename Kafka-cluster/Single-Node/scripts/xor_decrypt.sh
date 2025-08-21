16ab6783151db750f15bfb278c6974229b26c911
Praveen-2410 <chintalavenkatapraveenkumar@gmail.com>
2025-08-21 14:47:21 +0530

    Update xor_decrypt.sh

--- Kafka-cluster/Single-Node/scripts/xor_decrypt.sh
+++ Kafka-cluster/Single-Node/scripts/xor_decrypt.sh
@@ -6,9 +6,9 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 REMOTE_DIR="$(dirname "$(dirname "$SCRIPT_DIR")")"
  
 # Input encrypted passwords file
-ENCRYPTED_FILE="$REMOTE_DIR/config/kafka_encrypted_jaas.conf"
+ENCRYPTED_FILE="$REMOTE_DIR/shared/jaas/kafka_encrypted_jaas.conf"
 # Output decrypted JAAS file
-DECRYPTED_FILE="$REMOTE_DIR/config/kafka_decrypted_jaas.conf"
+DECRYPTED_FILE="$REMOTE_DIR/shared/jaas/kafka_decrypted_jaas.conf"
  
 tmpfile=$(mktemp)
  
@@ -27,7 +27,7 @@ while IFS= read -r line; do
  
     echo "Decrypting password for $user..."
  
-    dec_pwd=$(java -cp "$REMOTE_DIR/xor_encrypt_decrypt/PasswordUtility.jar:$REMOTE_DIR/xor_encrypt_decrypt/xercesImpl.jar" \
+    dec_pwd=$(java -cp "$REMOTE_DIR/shared/xor_encrypt_decrypt/PasswordUtility.jar:$REMOTE_DIR/shared/xor_encrypt_decrypt/xercesImpl.jar" \
         com.telcordia.util.PasswordEncryptor "d" "$enc")
  
     if [[ $count -eq $total_users ]]; then
